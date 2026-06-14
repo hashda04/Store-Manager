5f37f37225f79954a9b1e5ce8b566f70ccff94b9
hashda04 <hashidasherin@gmail.com>
2026-06-14 16:22:04 +0530

    frontend update

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://10.97.167.203:8000",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000",
 });
 
 export interface Customer {
